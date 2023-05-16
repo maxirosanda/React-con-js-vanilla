f2cd63a66c43e977807655417430e0dfea3767fa
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-05-15 23:56:14 -0300

    1 file modified, 2 added

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
+import React from "./react.js";
+import ReactDOM from  "./react-dom.js";
+const nombre = "maxi";
 
+const Buttons = () => {
+const style = {background:"#000"};
+    return (
+        <div style={style}>
+            <p>{nombre}</p>
+            <button data-id="btn1">Hola</button>
+            <button data-id="btn2">Hola</button>
+            <button data-id="btn3">Hola</button>
+            <button data-id="btn4">Hola</button>
+        </div>
+    )
+}
 const app = document.getElementById("app")
 const root = ReactDOM.createRoot(app)
-const nombre = "maxi";
-const style = {
-    background: "#000"
-};
-React.createElement("div", {
-    style: style
-}, React.createElement("p", null, nombre), React.createElement("button", {
-    "data-id": "btn1"
-}, "Hola"), React.createElement("button", {
-    "data-id": "btn2"
-}, "Hola"), React.createElement("button", {
-    "data-id": "btn3"
-}, "Hola"), React.createElement("button", {
-    "data-id": "btn4"
-}, "Hola"));
-root.render()
+root.render(Buttons)
