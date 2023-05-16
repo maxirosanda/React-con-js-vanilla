bd6d58c71caa4f0aa80bc8d6d7985605dec57434
maxirosanda <maxi_rosanda@hotmail.com>
2023-05-16 16:01:21 -0300

    estilos en hojas aparte

--- src/app.js
+++ src/app.js
@@ -4,12 +4,8 @@ import Nav from "./components/nav.js"
 import Home from "./components/home.js"
 import Blogs from './components/blogs.js'
 
-export const App = ()=>{
+const App = ()=>{
     const [nombre, setNombre] = React.useState("Maxi");
-    const style = {
-        background: "#000",
-        width: "100%"
-    };
     return React.createElement(BrowserRouter, null, React.createElement(Nav, {
         nombre: nombre
     }), React.createElement(Routes, null, React.createElement(Route, {
@@ -20,3 +16,4 @@ export const App = ()=>{
         element: React.createElement(Home, null)
     })));
 };
+export default App
