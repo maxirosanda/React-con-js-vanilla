e7b373f7fa448dac1493ca3945ad6f0411c7abca
maxirosanda <maxi_rosanda@hotmail.com>
2023-05-16 00:35:54 -0300

    app

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-import React from "./react.js";
-import ReactDOM from  "./react-dom.js";
-const nombre = "maxi";
-
-const Buttons = () => {
-const style = {background:"#000"};
-    return (
-        <div style={style}>
-            <p>{nombre}</p>
-            <button data-id="btn1">Hola</button>
-            <button data-id="btn2">Hola</button>
-            <button data-id="btn3">Hola</button>
-            <button data-id="btn4">Hola</button>
-        </div>
-    )
-}
-const app = document.getElementById("app")
-const root = ReactDOM.createRoot(app)
-root.render(Buttons)
+const App = ()=>{
+    const [nombre, setNombre] = React.useState("Maxi");
+    const style = {
+        background: "#000",
+        width: "100%"
+    };
+    return React.createElement(React.Fragment, null, React.createElement("p", null, nombre), React.createElement("div", {
+        style: style
+    }, React.createElement("button", {
+        "data-id": "btn1"
+    }, "Hola"), React.createElement("button", {
+        "data-id": "btn2"
+    }, "Hola"), React.createElement("button", {
+        "data-id": "btn3"
+    }, "Hola"), React.createElement("button", {
+        "data-id": "btn4"
+    }, "Hola")));
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App, null));
